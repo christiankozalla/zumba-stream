96cecb4e3008e805ed8a4942d59efd715b8af845
Christian Kozalla <christian.kozalla@gmail.com>
2023-08-22 20:36:08 +0200

    feat: use @sanity/astro instead of astro-sanity package

--- frontend/src/lib/urlForImage.js
+++ frontend/src/lib/urlForImage.js
@@ -1,6 +1,7 @@
-import { useSanityClient, createImageBuilder } from 'astro-sanity';
+import { useSanityClient } from "@sanity/astro";
+import imageUrlBuilder from "@sanity/image-url";
 
-export const imageBuilder = createImageBuilder(useSanityClient());
+export const imageBuilder = imageUrlBuilder(useSanityClient());
 
 export function urlForImage(source) {
   if (source === undefined) return { url: () => "" };
