d328814e1dfbc061f15b67086cd77a3c76e64e01
Christian Kozalla <christian.kozalla@gmail.com>
2023-10-09 09:16:21 +0200

    fix: urlForImage

--- frontend/src/lib/urlForImage.js
+++ frontend/src/lib/urlForImage.js
@@ -3,7 +3,12 @@ import imageUrlBuilder from "@sanity/image-url";
 
 export const imageBuilder = imageUrlBuilder(useSanityClient());
 
+function mockImageBuilder() {}
+
+mockImageBuilder.format = () => this;
+mockImageBuilder.url = () => "";
+
 export function urlForImage(source) {
-  if (source === undefined) return { url: () => "" };
+  if (source === undefined) return mockImageBuilder;
   return imageBuilder.image(source);
 }
