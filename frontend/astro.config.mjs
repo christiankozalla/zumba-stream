1a64ffa3bc6d7682ae486d99e24f4ed2f8c7bfb2
Magpie <anna.maria.weinhold@posteo.de>
2023-11-15 11:09:14 +0300

    price page renewed

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import sanity from "@sanity/astro";
+import {sanityIntegration} from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity({
+  integrations: [sanityIntegration({
     projectId: '807ohy6c',
     dataset: 'production',
     apiVersion: '2023-02-08',
